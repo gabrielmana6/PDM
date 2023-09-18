53c07298a488c3c040ce2254b778d6ad0128cd99
gabrielmana6 <gabrielmanasses13@gmail.com>
2023-09-18 17:05:19 -0300

    pratica01 finalizada

--- Pratica01/app/src/main/java/com/example/pratica01/MainActivity.kt
+++ Pratica01/app/src/main/java/com/example/pratica01/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.pratica01
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var textViewMensagem: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        this.textViewMensagem = findViewById(R.id.tvMensagem)
+
+        this.textViewMensagem.text = Aposta().toString()
     }
 }
