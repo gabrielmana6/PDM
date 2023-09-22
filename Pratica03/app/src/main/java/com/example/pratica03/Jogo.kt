b9e318942c7838a52ccbde6def81370780a1b98a
gabrielmana6 <gabrielmanasses13@gmail.com>
2023-09-22 02:16:44 -0300

    correcao imgs

--- Pratica03/app/src/main/java/com/example/pratica03/Jogo.kt
+++ Pratica03/app/src/main/java/com/example/pratica03/Jogo.kt
@@ -1,4 +1,14 @@
 package com.example.pratica03
 
 class Jogo {
+    private val cartas: Array<String>
+    private val tentativas: Int = 4
+
+    constructor(cartas: Array<String>) {
+        this.cartas = cartas
+    }
+
+    fun embaralharCartas(){
+
+    }
 }
